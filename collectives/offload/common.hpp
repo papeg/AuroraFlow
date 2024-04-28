8009250db1022667fbe34b9743a27dc92b436d71
Gerrit Pape <papeg@mail.upb.de>
2024-04-28 20:58:51 +0200

    protocol section in fork_array_dest

--- collectives/offload/common.hpp
+++ collectives/offload/common.hpp
@@ -26,10 +26,15 @@ fork_array_loop:
 
 inline void fork_array_dest(stream_word_union_t &header, STREAM<stream_word> &in, STREAM<stream_word> &out_0, uint32_t dest_0, STREAM<stream_word> &out_1, uint32_t dest_1)
 {
+io_section:
+{
+#pragma HLS pipeline fixed
     header.header.dest = dest_0;
     out_0.write(header.word_data);
+    ap_wait();
     header.header.dest = dest_1;
     out_1.write(header.word_data);
+}
 fork_array_dest_loop:
     for (uint32_t i = 0; i < header.header.count; i++)
     {
