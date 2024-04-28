b32d32727e73348a0a75a999b48a6db2ceffeafc
Gerrit Pape <papeg@mail.upb.de>
2024-04-28 20:58:36 +0200

    remove write_array optimization

--- collectives/collectives.hpp
+++ collectives/collectives.hpp
@@ -188,21 +188,12 @@ inline void write_array(STREAM<stream_word> &out, T *values, uint32_t count)
 {
     uint32_t width = get_width<T>();
 write_array_loop:
-    for (uint32_t i = 0; i < count; i += 2)
+    for (uint32_t i = 0; i < count; i++)
     {
-#pragma HLS protocol fixed
 #pragma HLS pipeline II=2
-// not enough memory ports for II=1
         stream_word_union_t data_0;
         pack_word(data_0, values + (i * width));
         out.write(data_0.word_data);
-// handle case when (count % 2 == 1)
-        if (i < count)
-        {
-            stream_word_union_t data_1;
-            pack_word(data_1, values + ((i + 1) * width));
-            out.write(data_1.word_data);
-        }
     }
 }
 
