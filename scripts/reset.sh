16aae8f1be3466a5c90b3683962e8dd16cec6e09
Gerrit Pape <papeg@mail.upb.de>
2024-11-21 09:55:59 +0100

    change reset mechanism

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-xbutil reset --force --device 0000:a1:00.1 && xbutil reset --force --device 0000:81:00.1 && xbutil reset --force --device 0000:01:00.1
+sudo /opt/software/FPGA/scripts/u280/reboot_single_fpga.sh 0000:a1:00.1 &&  sudo /opt/software/FPGA/scripts/u280/reboot_single_fpga.sh 0000:81:00.1 && sudo /opt/software/FPGA/scripts/u280/reboot_single_fpga.sh 0000:01:00.1
